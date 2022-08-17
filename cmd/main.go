afe26a0ffe35040815f88cb36c8c3584ec448bf5
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-08-17 22:13:59 +0700

    Buat cron untuk daily hubstaff
    Cron daily hubstaff dijadwalkan setiap hari pada jam 09:30

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"kitty-bot/cmd/app"
+	"kitty-bot/internal/helper"
 	"time"
 
 	"github.com/go-co-op/gocron"
@@ -11,12 +12,18 @@ import (
 func main() {
 	s := gocron.NewScheduler(time.Local)
 	request := resty.New().R()
+	cacheHelper := helper.NewCacheHelper()
 
 	// daily reminder scrum
 	s.Every(1).Day().At("16:28").Do(func() {
 		app.StartDailyScrum(request)
 	})
 
+	// daily hubstaff
+	s.Every(1).Day().At("09:30").Do(func() {
+		app.StartDailyHubstaff(request, *cacheHelper)
+	})
+
 	// start cron
-	// s.StartBlocking()
+	s.StartBlocking()
 }
