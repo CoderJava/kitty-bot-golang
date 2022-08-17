2d26ebf7cb5d2682663d83e82642301665cc0785
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-08-17 08:26:52 +0700

    Buat file env_key.go
    File tersebut berfungsi sebagai kumpulan dari semua key yang ada didalam file .env

---
+++ configs/env_key.go
@@ -0,0 +1,41 @@
+package configs
+
+// app env
+var AppEnv = "APP_ENV"
+
+// hubstaff auth
+var RefreshToken = "REFRESH_TOKEN"
+
+// ID discord channel
+var IdChannelDiscordProduction = "ID_CHANNEL_DISCORD_PRODUCTION"
+var IdChannelDiscordDevelopment = "ID_CHANNEL_DISCORD_DEVELOPMENT"
+
+// token bot discord
+var DiscordBotToken = "DISCORD_BOT_TOKEN"
+
+// list id discord user
+var IdDiscordAdeIskandar = "ID_DISCORD_ADE_ISKANDAR"
+var IdDiscordAldoFaizi = "ID_DISCORD_ALDO_FAIZI"
+var IdDiscordAbdulAziz = "ID_DISCORD_ABDUL_AZIZ"
+var IdDiscordBobby = "ID_DISCORD_ADHITYA_BOBBY"
+var IdDiscordYudiSetiawan = "ID_DISCORD_YUDI_SETIAWAN"
+var IdDiscordDewi = "ID_DISCORD_DEWI_LILIAN"
+var IdDiscordRioDwi = "ID_DISCORD_RIO_DWI_PRABOWO"
+var IdDiscordSabrino = "ID_DISCORD_SABRINO"
+var IdDiscordRyan = "ID_DISCORD_RYAN_ALFARISI"
+var IdDiscordAditama = "ID_DISCORD_ADITAMA"
+
+// list id hubstaff user
+var IdHubstaffAdeIskandar = "ID_HUBSTAFF_ADE_ISKANDAR"
+var IdHubstaffYudiSetiawan = "ID_HUBSTAFF_YUDI_SETIAWAN"
+var IdHubstaffRyan = "ID_HUBSTAFF_RYAN_ALFARISI"
+var IdHubstaffSabrino = "ID_HUBSTAFF_SABRINO"
+var IdHubstaffRioDwi = "ID_HUBSTAFF_RIO_DWI_PRABOWO"
+var IdHubstaffBobby = "ID_HUBSTAFF_ADHITYA_BOBBY"
+var IdHubstaffAditama = "ID_HUBSTAFF_ADITAMA"
+var IdHubstaffAldoFaiz = "ID_HUBSTAFF_ALDO_FAIZI"
+var IdHubstaffDewi = "ID_HUBSTAFF_DEWI_LILIAN"
+var IdHubstaffAbdulAziz = "ID_HUBSTAFF_ABDUL_AZIZ"
+
+// base api
+var BaseApiDiscord = "BASE_API_DISCORD"
