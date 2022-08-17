ae6b7a6e5a58a8ce3698428bf8506c92727bb69b
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-08-17 22:09:52 +0700

    Update client endpoint hubstaff
    Update agar bagian auth-nya sudah menggunakan cache untuk penyimpanan refresh token. Dan bagian hubstaff-nya sudah menggunakan header authorization.

--- cmd/datasource/hubstaff_remote_data_source.go
+++ cmd/datasource/hubstaff_remote_data_source.go
@@ -13,26 +13,34 @@ type hubstaffRemoteDataSource struct {
 	request     *resty.Request
 	baseApi     string
 	baseApiAuth string
+	cacheHelper helper.CacheHelper
 }
 
-func NewHubstaffRemoteDataSource(request *resty.Request) *hubstaffRemoteDataSource {
+func NewHubstaffRemoteDataSource(
+	request *resty.Request,
+	cacheHelper helper.CacheHelper,
+) *hubstaffRemoteDataSource {
 	baseApi := helper.LoadEnvVariable(configs.BaseApiHubstaff)
 	baseApiAuth := helper.LoadEnvVariable(configs.BaseApiAuthHubstaff)
 	return &hubstaffRemoteDataSource{
 		request:     request,
 		baseApi:     baseApi,
 		baseApiAuth: baseApiAuth,
+		cacheHelper: cacheHelper,
 	}
 }
 
 func (r hubstaffRemoteDataSource) Login() (loginResponse hubstaff.LoginResponse) {
 	path := r.baseApiAuth + "access_tokens"
-	refreshToken := helper.LoadEnvVariable(configs.RefreshToken)
+	refreshToken := r.cacheHelper.Get(configs.RefreshToken)
+	if refreshToken == "" {
+		refreshToken = helper.LoadEnvVariable(configs.RefreshToken)
+	}
 	_, err := r.request.
 		SetHeader("Content-Type", "application/json").
 		SetQueryParam("grant_type", "refresh_token").
 		SetQueryParam("refresh_token", refreshToken).
-		SetResult(loginResponse).
+		SetResult(&loginResponse).
 		Post(path)
 	if err != nil {
 		log.Fatal(helper.SprintLog("login hubstaff failed"))
@@ -40,10 +48,35 @@ func (r hubstaffRemoteDataSource) Login() (loginResponse hubstaff.LoginResponse)
 	return
 }
 
-/* func (r hubstaffRemoteDataSource) GetListMembers() (membeResponse hubstaff.MemberResponse) {
+func (r hubstaffRemoteDataSource) GetListMembers() (membeResponse hubstaff.MemberResponse) {
 	path := r.baseApi + "projects/671717/members"
+	accessToken := r.cacheHelper.Get(configs.AccessToken)
 	_, err := r.request.
-		SetHeader("Content-Type", "application/json")
-
+		SetHeader("Content-Type", "application/json").
+		SetHeader("Authorization", "Bearer "+accessToken).
+		SetResult(&membeResponse).
+		Get(path)
+	if err != nil {
+		log.Fatal(helper.SprintLog("get list members failed"))
+	}
 	return
-} */
+}
+
+func (r hubstaffRemoteDataSource) GetDailyActivityByRangeDate(
+	startDate string,
+	stopDate string,
+) (dailyActivityResponse hubstaff.DailyActivityResponse) {
+	path := r.baseApi + "organizations/166190/activities/daily"
+	accessToken := r.cacheHelper.Get(configs.AccessToken)
+	_, err := r.request.
+		SetHeader("Content-Type", "application/json").
+		SetHeader("Authorization", "Bearer "+accessToken).
+		SetQueryParam("date[start]", startDate).
+		SetQueryParam("date[stop]", stopDate).
+		SetResult(&dailyActivityResponse).
+		Get(path)
+	if err != nil {
+		log.Fatal(helper.SprintLog("get daily activity by range date failed"))
+	}
+	return
+}
